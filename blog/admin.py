1d88d54736d429e8980a771eb158ba0f055bf79f
choi <boy0516@naver.com>
2021-01-14 19:38:54 +0900

    업뎃

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 # admin page 출력되는 내용을 customise
 class PostAdmin(admin.ModelAdmin):
@@ -17,4 +17,5 @@ class PostAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Post, PostAdmin)
+admin.site.register(Comment)
 # Register your models here.
